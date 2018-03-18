84e527c27fb6b1933a0d356b6aea5de19b9d0024
Miguel Angel Fernandez <mafesan@bitergia.com>
2018-03-18 10:41:15 +0100

    Change print messages to logging messages

--- github-api.py
+++ github-api.py
@@ -38,6 +38,7 @@ DESC_MSG = 'Extracts git trees (list of files) from GitHub repositories'
 
 def main(args):
 
+    logger.info('GitHub-API starts...')
     github_key = args.github_token
     ProjectRecord = namedtuple('ProjectRecord', 'id, url, owner_id, name, descriptor, language, created_at, forked_from, deleted, updated_at')
 
@@ -64,13 +65,13 @@ def main(args):
             sha_hash = read_json(repo, "master", ["commit", "commit", "tree", "sha"])
 
             if not sha_hash:
-                print("Master branch not found: " + repo.url)
+                logger.debug("Master branch not found: %s", repo.url)
                 if not get_json(repo, github_key, "default"):
                     continue
                 default = read_json(repo, "default", ["default_branch"])
 
                 if not default:
-                    print("No default branch found: " + repo.url)
+                    logger.debug("No default branch found: %s", repo.url)
                     time.sleep(1.40)
                     continue
                 if not get_json(repo, github_key, "master", "/branches/" + default):
@@ -78,14 +79,14 @@ def main(args):
                 sha_hash = read_json(repo, "master", ["commit", "commit", "tree", "sha"])
 
                 if not sha_hash:
-                    print("Default branch not found: " + repo.url)
+                    logger.debug("Default branch not found: %s", repo.url)
                     time.sleep(2.10)
                     continue
             if not get_json(repo, github_key, "trees", "/git/trees/" + sha_hash + "?recursive=1"):
                 continue
             time.sleep(1.40)
 
-    print("End of program")
+    logger.info("End of program")
 
 
 def lookup(dic, key, *keys):
@@ -116,14 +117,16 @@ def get_json(repo, github_key, directory, url_append=""):
     else:
         url = url + "?" + github_key
     try:
-        print("Retrieve: ", url)
-    except UnicodeEncodeError:
-        print("UnicodeEncodeError:" + str(repo) + directory)
+        logger.info("Retrieve: %s", repo.url)
+    except UnicodeEncodeError as e:
+        logger.debug("%s, %s", str(e), str(repo))
+        logger.debug("directory: %s", directory)
         return 0
     try:
-        urllib.request.urlretrieve(url, directory + "/" + repo.owner_id + ":" + repo.id + ".json")
-    except IOError:
-        print("IOERROR: " + url)
+        json_name = "%s/%s:%s.json" % (directory, str(repo.owner_id), str(repo.id))
+        urllib.request.urlretrieve(url, json_name)
+    except IOError as e:
+        logger.debug("%s, url: %s", str(e), url)
         return 0
     return 1
 
@@ -135,14 +138,13 @@ def read_json(repo, directory, lookup_list):
     it looks up for a given value in the JSON (given as a list)
     and returns its value
     """
-    with open(directory + "/" + repo.owner_id + ":" + repo.id + ".json") as data_file:
+    json_name = "%s/%s:%s.json" % (directory, str(repo.owner_id), str(repo.id))
+    with open(json_name) as data_file:
         try:
             data = json.load(data_file)
-        except ValueError:
-            message = "Error with file: " + directory + "/" + repo.owner_id
-            message += ":" + repo.id + ".json"
-            print(message)
-
+        except ValueError as e:
+            logger.error(str(e))
+            logger.debug("Error with file: %s", json_name)
     try:
         return lookup(data, *lookup_list)
     except KeyError:
@@ -208,6 +210,7 @@ def parse_args():
 if __name__ == '__main__':
     # TODO: Add documentation
     # TODO: Add new methods to reduce main
+    # TODO: Check if response is truncated
     try:
         args = parse_args()
         keep_fds = configure_logging(args.log_file, args.debug_mode_on)
